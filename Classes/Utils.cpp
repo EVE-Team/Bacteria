52446f80f5aaaeb95a244a2501e9798d55677de7
bogdanov-d-a <bogdanov.d.a.1994@gmail.com>
2016-06-03 15:01:40 +0300

    Fix plankton acceleration

--- Classes/Utils.cpp
+++ Classes/Utils.cpp
@@ -30,7 +30,7 @@ Vec2 Utils::RandVec2()
 const Size Utils::fieldSize = Size(2000, 2000);
 const float Utils::planktonAreaMul = 0.05f;
 const float Utils::bacteriaPushForce = 200;
-const float Utils::planktonPushForceMul = -0.1f;
+const float Utils::planktonPushForceMul = -1;
 const float Utils::minBacteriaRadius = 25;
 
 Utils::PolarVec2 Utils::LinearToPolar(Vec2 lin)
